6bfc663aec203a7c7f19cbee7c48800c8233ce88
randomGui44 <randomGui44@users.noreply.gitlab.com>
2021-06-17 22:06:30 -0400

    Corrected further hard-coded paths

--- src/QuoteEngine/IngestPDF.py
+++ src/QuoteEngine/IngestPDF.py
@@ -18,14 +18,14 @@ class IngestPDF(IngestorInterface):
         except Exception:
             raise Exception("Cannot Ingest Exception")
 
-        tmp_file = f'./src/{random.randint(0,10000)}.txt'
-        program = "./src/xpdf-tools-win-4.03/bin64/pdftotext.exe"
+        tmp_file = f'./{random.randint(0,10000)}.txt'
+        program = "./xpdf-tools-win-4.03/bin64/pdftotext.exe"
         subprocess.call([program, path, tmp_file])
 
         data_file = open(tmp_file, 'r')
         quotes = []
         for line in data_file.readlines():
-            line = line.strip('\n\r').strip()
+            line = line.strip('/n/r').strip()
             if len(line) > 0:
                 parsed_line = line.split('-')
                 new_quote = QuoteModel(parsed_line[0], parsed_line[1])
